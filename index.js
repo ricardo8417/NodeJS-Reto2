46eeabfcb022b23a83cd731b36f15009cabc09da
Ricardo <rnavarrete_cervantes@hotmail.com>
2022-04-06 22:37:22 -0500

    Reto 2 nodejs

---
+++ index.js
@@ -0,0 +1,6 @@
+// var colors = require('colors');
+// console.log('Saludos Grupo'.rainbow);
+
+
+var Restar = require('./restar');
+Restar(6, 2);
---
+++ restar.js
@@ -0,0 +1,4 @@
+function restar(num1, num2) {
+    console.log(num1 - num2)
+}
+module.exports = restar;
